f8f148cbbba5d4a26dd9d46cea95ed6336c31f80
Seanbon0611 <sean.dever@datadoghq.com>
2023-03-29 13:14:26 -0700

    adding rum and trace correlation

--- rum-go/main.go
+++ rum-go/main.go
@@ -14,9 +14,10 @@ type Data struct {
 
 func main() {
 	tracer.Start(
-		tracer.WithEnv("prod"),
-		tracer.WithService("go_rum"),
+		tracer.WithEnv("sandbox"),
+		tracer.WithService("test_service"),
 		tracer.WithServiceVersion("0.0.1"),
+		tracer.WithDebugMode(true),
 	)
 	defer tracer.Stop()
 	mux := httptrace.NewServeMux()
@@ -26,11 +27,6 @@ func main() {
 
 func getMessage(w http.ResponseWriter, r *http.Request) {
 	message := Data{Message: "Hello, World!!!!!!"}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, x-datadog-origin, x-datadog-sampling-priority, x-datadog-parent-id, x-datadog-trace-id")
 
 	json.NewEncoder(w).Encode(message)
 }
